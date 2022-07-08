e1d3d8469b908d06e8c31bd20ec6561952294573
krolliaa <krolliaa@163.com>
2022-07-08 16:17:36 +0800

    4. 使用 https://start.aliyun.com 官网 创建 SpringBoot 项目

---
+++ SpringBoot_demo01_04/src/main/java/com/kk/SpringBootDemo0104Application.java
@@ -0,0 +1,13 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo0104Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo0104Application.class, args);
+    }
+
+}
