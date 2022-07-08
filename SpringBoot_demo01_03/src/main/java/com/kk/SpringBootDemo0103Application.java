44f51746111dde882ec476ad36ea987dad9a0415
krolliaa <krolliaa@163.com>
2022-07-08 15:43:19 +0800

    3. 手动创建 SpringBoot 项目

---
+++ SpringBoot_demo01_03/src/main/java/com/kk/SpringBootDemo0103Application.java
@@ -0,0 +1,11 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo0103Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo0103Application.class, args);
+    }
+}
---
+++ SpringBoot_demo01_03/src/main/java/com/kk/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.kk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+    @GetMapping
+    public String getById() {
+        System.out.println("SpringBoot Run 03...");
+        return "SpringBoot Run 03...";
+    }
+}
