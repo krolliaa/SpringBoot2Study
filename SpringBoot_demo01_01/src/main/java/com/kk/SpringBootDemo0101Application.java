bbb5ee3a248fee594f638b9bba28640ba9fc9ac3
krolliaa <krolliaa@163.com>
2022-07-08 15:29:32 +0800

    1. 使用 https://start.spring.io IDEA 创建 SpringBoot 项目

---
+++ SpringBoot_demo01_01/src/main/java/com/kk/SpringBootDemo0101Application.java
@@ -0,0 +1,13 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo0101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo0101Application.class, args);
+    }
+
+}
