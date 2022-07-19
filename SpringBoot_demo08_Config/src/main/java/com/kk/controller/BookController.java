ca81ad1640b9c84aa56f2bfb5275d9d45df15e3c
krolliaa <krolliaa@163.com>
2022-07-19 18:14:49 +0800

    多环境配置： 1. yaml 版本使用单个文件进行多环境配置 2. yaml 版本使用多个文件进行多环境配置

--- SpringBoot_demo08_Config/src/main/java/com/kk/controller/BookController.java
+++ SpringBoot_demo08_Config/src/main/java/com/kk/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.kk.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/books")
 public class BookController {
-
-    @Value(value = "${a}")
-    private Integer price;
-
     @GetMapping
     public String getById() {
         System.out.println("SpringBoot Run...");
-        return "SpringBoot Run..." + price;
+        return "SpringBoot Run...";
     }
 }
